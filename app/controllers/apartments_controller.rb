439adfa3f2a0fd9acf35206b24fcdee00825574e
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-10-29 09:25:45 -0700

    create functionality

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -4,20 +4,16 @@ class ApartmentsController < ApplicationController
         render json: apartments
     end
 
-    
-end
-=begin
-def create
-    apartment = Apartment.create(apartment_params)
-    if apartment.valid?
-        render json: apartment
-    else
-        render json: apartment.errors, status: 422
+    def create 
+        apartment = Apartment.create(apartment_params)
+        if apartment.valid?
+            render json: apartment
+        else
+            render json: apartment.errors, status: 422
+        end
     end
+     private
+        def apartment_params
+         params.require(:apartment).permit(:street, :unit, :city, :state, :square_footage, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
+     end
 end
-private
-def apartment_params
-    params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
-end
-
-=end
